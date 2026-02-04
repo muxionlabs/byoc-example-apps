b965f917ef87034f3b91ae5eb4a4236cc8d0afbb
Jason <83615043+JJassonn69@users.noreply.github.com>
2026-02-04 16:06:04 +0200

    feat: add pytrickle projects to the website (#11)
    Add pytrickle example projects.
    
    ---------
    
    Co-authored-by: Rick Staa <rick.staa@outlook.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${heading.variable} ${body.variable} dark`}>
+    <html
+      lang="en"
+      className={`${heading.variable} ${body.variable} dark`}
+    >
       <body>{children}</body>
     </html>
   );
